336b999614bb3bc1a7fe5470f8279bdbe88b2b37
Mathias Buus <mathiasbuus@gmail.com>
2018-01-13 14:22:34 +0100

    add failing timeout test

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
 var tape = require('tape')
+var dgram = require('dgram')
 var dns = require('./')
 
 tape('query + response', function (t) {
@@ -28,6 +29,102 @@ tape('query + response', function (t) {
       t.same(res.answers[0].name, 'test')
       t.same(res.answers[0].data, '1.1.1.1')
       t.end()
+    })
+  })
+})
+
+tape('pass socket + query + response', function (t) {
+  var udp = dgram.createSocket('udp4')
+
+  udp.bind(0, function () {
+    var socket = dns({socket: udp})
+
+    socket.on('query', function (query, port, host) {
+      socket.response(query, {
+        answers: [{
+          type: 'A',
+          name: 'test',
+          data: '1.1.1.1'
+        }]
+      }, port, host)
+    })
+
+    socket.query({
+      questions: [{
+        type: 'A',
+        name: 'test'
+      }]
+    }, socket.address().port, function (err, res) {
+      socket.destroy()
+      t.error(err)
+      t.same(res.answers.length, 1)
+      t.same(res.answers[0].type, 'A')
+      t.same(res.answers[0].name, 'test')
+      t.same(res.answers[0].data, '1.1.1.1')
+      t.end()
+    })
+  })
+})
+
+tape('timeout', function (t) {
+  var dummy = dgram.createSocket('udp4')
+
+  dummy.bind(0, function () {
+    var done = false
+    var timeout = setTimeout(function () {
+      done = true
+      socket.destroy()
+      dummy.close()
+      t.fail('should timeout before 10s')
+      t.end()
+    }, 10000)
+
+    var socket = dns()
+
+    var id = socket.query({
+      questions: [{type: 'A', name: 'test'}]
+    }, dummy.address().port, function (err) {
+      if (done) return
+      clearTimeout(timeout)
+      socket.destroy()
+      dummy.close()
+      t.ok(err, 'should error')
+      t.end()
+    })
+
+    socket.setRetries(id, 0)
+  })
+})
+
+tape('pass socket + timeout', function (t) {
+  var udp = dgram.createSocket('udp4')
+  var dummy = dgram.createSocket('udp4')
+
+  dummy.bind(0, function () {
+    udp.bind(0, function () {
+      var done = false
+      var timeout = setTimeout(function () {
+        done = true
+        socket.destroy()
+        dummy.close()
+        t.fail('should timeout before 10s')
+        t.end()
+      }, 10000)
+
+      var socket = dns({socket: udp})
+
+      var id = socket.query({
+        questions: [{type: 'A', name: 'test'}]
+      }, dummy.address().port, function (err) {
+        if (done) return
+        clearTimeout(timeout)
+        socket.destroy()
+        dummy.close()
+        t.ok(err, 'should error')
+        t.end()
+      })
+
+      socket.setRetries(id, 0)
     })
   })
 })
